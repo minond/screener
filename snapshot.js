44ed2814eb3dca9be95829ad96c054958a3bb2a9
Marcos Minond <minond.marcos@gmail.com>
2014-06-01 20:10:32 -0600

    test file passed as parameter

--- snapshot.js
+++ snapshot.js
@@ -3,17 +3,24 @@
 /* global phantom */
 
 var DEFAULT_OUT_DIR = '.screener';
+var DEFAULT_TESTS_FILE = 'tests.json';
+
+var tests;
 
 var webpage = require('webpage'),
     system = require('system'),
     lodash = require('lodash'),
-    tests = require('./tests.json'),
+    fs = require('fs'),
     log = require('./lib/log.js'),
     runner = require('./lib/runner.js');
 
 var page = webpage.create(),
-    dir = system.args[1] || DEFAULT_OUT_DIR,
+    file = system.args[1] || DEFAULT_TESTS_FILE,
+    dir = system.args[2] || DEFAULT_OUT_DIR,
     normalized = {};
+
+// require as local file
+tests = require('.' + fs.separator + file);
 
 try {
     normalized = runner.normalize(tests);
@@ -26,4 +33,3 @@ lodash.each(normalized, function (info, label) {
     log.debug('testing', label);
     runner.execute(label, dir, info.url, info.executions, page, phantom);
 });
-
