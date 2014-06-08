e983b94ecc21f440b360321f52d9398b1306876d
Marcos Minond <minond.marcos@gmail.com>
2014-06-08 14:47:44 -0600

    multiple definitions on .json file

--- snapshot.js
+++ snapshot.js
@@ -1,11 +1,13 @@
 'use strict';
 
+/**
+ * @usage phantomjs snapshot.js [tests] [output]
+ * loads tests file and executes and saves them
+ */
+
 /* global phantom */
-
-var DEFAULT_OUT_DIR = '.screener';
-var DEFAULT_TESTS_FILE = 'tests.json';
-
-var tests;
+var DEFAULT_OUT_DIR = '.screener',
+    DEFAULT_TESTS_FILE = 'tests.json';
 
 var webpage = require('webpage'),
     system = require('system'),
@@ -14,22 +16,42 @@ var webpage = require('webpage'),
     log = require('./lib/log.js'),
     runner = require('./lib/runner.js');
 
-var page = webpage.create(),
-    file = system.args[1] || DEFAULT_TESTS_FILE,
+var file = system.args[1] || DEFAULT_TESTS_FILE,
     dir = system.args[2] || DEFAULT_OUT_DIR,
-    normalized = {};
+    normalized = {},
+    tests = {};
 
-// require as local file
-tests = require('.' + fs.separator + file);
+var test_count = 0,
+    fail_count = 0;
 
 try {
+    tests = require('.' + fs.separator + file);
     normalized = runner.normalize(tests);
 } catch (err) {
     log.error(err.message);
     phantom.exit(1);
 }
 
-lodash.each(normalized, function (test, label) {
-    log.debug('testing', label);
-    runner.execute(label, dir, test, page, phantom);
-});
+(function next() {
+    var tests = Object.keys(normalized),
+        test = tests.shift(),
+        data = normalized[ test ],
+        page = webpage.create();
+
+    if (test_count) {
+        log.debug('');
+    }
+
+    if (test) {
+        test_count++;
+        log.debug('testing', test);
+        delete normalized[ test ];
+        runner.execute(test, dir, data, page, next, function () {
+            fail_count++;
+            next();
+        });
+    } else {
+        log.debug(test_count - fail_count, 'out of', test_count, 'builds ok');
+        phantom.exit(fail_count);
+    }
+})();
